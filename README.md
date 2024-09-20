634d78b43fd8e581cb7352d3abf6b0d55d70d735
Harri Huhtala <bodhisatva@test>
2024-09-20 13:44:42 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # verify-sha-256
-A script to verify the SHA-256 checksum of a file.
+
+A Bash script to verify the SHA-256 checksum of a file.
---
+++ verify-shasum.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# a script to verify the SHA-256 checksum of a file
+
+# check if two parameters are provided
+if [ "$#" -ne 2 ]; then
+  echo -e "\nMissing a parameter"
+  echo -e "Usage: verify-shasum-256 <source_shasum> <file_to_be_verified>\n"
+  exit 1
+fi
+
+# source shasum
+source_shasum=$1
+
+# file to be calculated
+file=$2
+
+# function to show progress indicator
+show_progress() {
+  echo -e "\nComparing hashes:\n"
+    while true; do
+      printf "\e[33m#\e[0m"
+      sleep 1
+    done
+}
+
+# start progress indicator
+show_progress &
+progress_pid=$!
+
+# start progress indicator
+file_shasum=$(shasum -a 256 "$file" | awk '{ print $1 }')
+
+# end progress indicator
+kill $progress_pid
+echo
+
+if [ "$source_shasum" = "$file_shasum" ]; then
+ echo -e "\nSHA-256 checksum verification successful: Hash is a match.\n"
+else
+ echo -e "\nSHA-256 checksum verification failed: The hash does not match.\n" 
+fi
